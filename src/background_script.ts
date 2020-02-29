8ee11837a0c3b3337a811510f30409fb64df573c
Jiaming Shang <shangjiaming.yuan@gmail.com>
2020-02-29 11:46:02 +0800

    try execute script on tab active

--- src/background_script.ts
+++ src/background_script.ts
@@ -1,10 +1,12 @@
 import {turndownServie, copyToClipboard} from './types'
 
+let selectedTabId: number = -1
+
 const sendMessageToTab = () => {
   chrome.tabs.query({active: true}, (tabs) => {
     if (tabs[0]) {
       chrome.tabs.sendMessage(tabs[0].id as number, {'copy-as-markdown': true}, (response: {selection?: string}) => {
-        if (response.selection) {
+        if (response && response.selection) {
           const markdown = turndownServie.turndown(response.selection)
           copyToClipboard(markdown)
           console.log('Copied')
@@ -31,6 +33,25 @@ const onCommandTriggered = (command: string) => {
   }
 }
 
+const onTabUpdated = (tabId: number, changeInfo: chrome.tabs.TabChangeInfo, tab: chrome.tabs.Tab) => {
+  if (changeInfo.status == "complete" && tabId == selectedTabId) {
+    //chrome.tabs.executeScript(tabId, {file: chrome.runtime.getURL('js/content_script.bundle.js')}, (result: any) => {
+    //console.log("Inject script result:")
+    //console.log(result)
+    //})
+    console.log(changeInfo)
+    console.log(tab)
+    chrome.tabs.executeScript(tabId, {code: 'console.log("injected")'}, (result: any) => {
+      console.log("Inject script result:")
+      console.log(result)
+    })
+  }
+}
+
+const onTabActivated = (activeInfo: chrome.tabs.TabActiveInfo) => {
+  selectedTabId = activeInfo.tabId
+}
+
 const initBackgroundScript = () => {
   console.log('background running');
   chrome.runtime.onInstalled.addListener(() => {
@@ -42,6 +63,14 @@ const initBackgroundScript = () => {
     chrome.contextMenus.onClicked.addListener(onContextMenuClicked)
 
     chrome.commands.onCommand.addListener(onCommandTriggered);
+
+    chrome.tabs.onUpdated.addListener(onTabUpdated)
+
+    chrome.tabs.onActivated.addListener(onTabActivated)
+  });
+
+  chrome.tabs.query({active: true, currentWindow: true}, (tabs: chrome.tabs.Tab[]) => {
+    selectedTabId = tabs[0].id as number;
   });
 }
 
